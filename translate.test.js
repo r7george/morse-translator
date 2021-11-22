ba841d03d8b8e586c4dd47566d00007ad1898b37
Reuben George <reuben_george1998@outlook.com>
2021-11-22 11:47:27 +0000

    base-testing-layout

---
+++ translate.test.js
@@ -0,0 +1,7 @@
+import { translator } from "./translate.js";
+import { it, expect, describe } from "@jest/globals";
+
+// Need to write tests to translate english to morse
+// Going to use "describe to group together tests"
+
+// Testing random letters to see if they translate correctly
