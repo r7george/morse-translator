9ed111dc5ad8c3c1aa214e0c150d578bec6d9bd8
Reuben George <reuben_george1998@outlook.com>
2021-11-22 12:25:12 +0000

    word-test-written

--- translate.test.js
+++ translate.test.js
@@ -6,6 +6,39 @@ import { it, expect, describe } from "@jest/globals";
 
 // Testing random letters to see if they translate correctly
 describe("Testing single letter translations", () => {
+  it("Should translate a to .-", () => {
+    const result = morseTranslator("a");
+    expect(result).toBe(".-");
+  });
+  
+  it("Should translate i to ..", () => {
+    const result = morseTranslator("i");
+    expect(result).toBe("..");
+  });
+
+  it("Should translate m to --", () => {
+    const result = morseTranslator("m");
+    expect(result).toBe("--");
+  });
+
+  it("Should translate q to --.-", () => {
+    const result = morseTranslator("q");
+    expect(result).toBe("--.-");
+  });
+
+  it("Should translate s to ...", () => {
+    const result = morseTranslator("s");
+    expect(result).toBe("...");
+  });
+
+  it("Should translate y to -.--", () => {
+    const result = morseTranslator("y");
+    expect(result).toBe("-.--");
+  });
+});
+
+// Testing random words to see if they translate correctly
+describe("Testing word translations", () => {
   it("Should translate hello to '.... . .-.. .-.. ---'", () => {
     const result = morseTranslator("hello");
     expect(result).toBe(".... . .-.. .-.. ---");
@@ -15,4 +48,5 @@ describe("Testing single letter translations", () => {
     const result = morseTranslator("world");
     expect(result).toBe(".-- --- .-. .-.. -..");
   });
+
 });
