e52d6b61c21a977f8f5587bad733bbcb4cea706a
Reuben George <reuben_george1998@outlook.com>
2021-11-23 13:01:53 +0000

    can-translate-both-ways

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,44 @@
-import { englishTranslator } from "./translate.js";
+import { englishTranslator, morseTranslator } from "./translate.js";
 
-const englishInput = document.querySelector("#translatorInput");
-const morseOutput = document.querySelector("#translatorOutput");
+const input = document.querySelector("#translatorInput");
+const output = document.querySelector("#translatorOutput");
+
 const submitBtn = document.querySelector("#submitBtn");
 const clearBtn = document.querySelector("#clearBtn");
+const switchBtn = document.querySelector("#switchBtn");
+
+const labels = document.querySelectorAll(".translator__label");
+
 
 const handleTranslateClick = () => {
-  const input = englishInput.value;
-  const output = englishTranslator(input);
-  morseOutput.value = output;
+  if (labels[0].innerHTML == "English") {
+    const inputJS = input.value;
+    const outputJS = englishTranslator(inputJS);
+    output.value = outputJS;
+  }
+  else {
+    const inputJS = input.value;
+    const outputJS = morseTranslator(inputJS);
+    output.value = outputJS;
+  }
 }
 
 const handleClearClick = () => {
-  englishInput.value = "";
-  morseOutput.value = "";
+  input.value = "";
+  output.value = "";
+}
+
+const handleSwitchClick = () => {
+  if(labels[0].innerHTML == "English") {
+    labels[0].innerHTML = "Morse";
+    labels[1].innerHTML = "English";
+  }
+  else if (labels[0].innerHTML == "Morse") {
+    labels[0].innerHTML = "English";
+    labels[1].innerHTML = "Morse";
+  }
 }
 
 submitBtn.addEventListener("click", handleTranslateClick);
 clearBtn.addEventListener("click", handleClearClick);
+switchBtn.addEventListener("click", handleSwitchClick);
